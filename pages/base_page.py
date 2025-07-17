6518247f14901cf36e8fb6b7c578b77f0b0c6882
Ilya <vyrodov.ilya@inbox.ru>
2025-07-17 16:46:30 +0300

    добавили проверки на то что элемент отсутствует

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,4 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -29,6 +29,7 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+            # метод, который проверяет, что элемент не появляется на странице в течение заданного времени
         def is_not_element_present(self, how, what, timeout=4):
             try:
                 WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
@@ -36,3 +37,12 @@ class BasePage():
                 return True
 
             return False
+        #будет ждать пока элемент не исчеснит
+        def is_disappeared(self, how, what, timeout=4):
+            try:
+                WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                    until_not(EC.presence_of_element_located((how, what)))
+            except TimeoutException:
+                return False
+
+            return True
